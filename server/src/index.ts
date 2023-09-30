99619f6fb0b06c9ec0fd69c6cd6a26905978c3f8
Melvin Valencia <3205672730501@ingenieria.usac.edu.gt>
2023-09-30 10:16:56 -0600

    Inicio server

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+const app = express()
+app.use(express.json())
+
+const PORT = 3000
+
+app.get('/', (_, res) => {
+    console.log('GET /')
+    res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at localhost:${PORT}`)
+})
